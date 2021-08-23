cd1d40c7762fecb7cee575d5a98f0b4df2f26e4e
Yuancheng Zhang <old.far.hot@gmail.com>
2021-08-23 20:43:52 +0800

    add 2 batch files

---
+++ tool_convert.bat
@@ -0,0 +1,6 @@
+:: Convert Excel to GDScript (Windows)
+:: @copyright Hidden Moss
+:: @see Hidden Moss: https://hiddenmoss.com/
+:: @see git repo: https://github.com/hidden-moss/xls2gd
+:: @author: Yuancheng Zhang, https://github.com/endaye
+powershell -command "python .\tool_xls2gd.py"
---
+++ tool_init.bat
@@ -0,0 +1,6 @@
+:: Initialize tool_xls2gd (Windows)
+:: @copyright Hidden Moss
+:: @see Hidden Moss: https://hiddenmoss.com/
+:: @see git repo: https://github.com/hidden-moss/xls2gd
+:: @author: Yuancheng Zhang, https://github.com/endaye
+pip install xlutils xlrd==1.2.0
