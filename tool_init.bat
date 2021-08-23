9975193837d3d6513ad2377809b3616d9405ea51
Yuancheng Zhang <old.far.hot@gmail.com>
2021-08-23 21:18:58 +0800

    update tool_init.bat

--- tool_init.bat
+++ tool_init.bat
@@ -3,4 +3,4 @@
 :: @see Hidden Moss: https://hiddenmoss.com/
 :: @see git repo: https://github.com/hidden-moss/xls2gd
 :: @author: Yuancheng Zhang, https://github.com/endaye
-pip install xlutils xlrd==1.2.0
+pip install xlutils xlrd==1.2.0 pylint pyinstaller wxpython
